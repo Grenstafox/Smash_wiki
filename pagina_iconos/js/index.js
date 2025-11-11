a5ea96c94c35e1be619bc1ccb155bcd4ed63d1ef
Gren <124205591+Grenstafox@users.noreply.github.com>
2025-11-10 23:55:17 -0700

    index.js

--- pagina_iconos/js/index.js
+++ pagina_iconos/js/index.js
@@ -1,109 +1,35 @@
-const card_place = document.querySelector("#card_place");
-const container = document.querySelector("#container");
-const container_saga = document.querySelector("#container_saga");
-const icon_saga = document.querySelector("#icon_saga");
-const saga_characters = document.querySelector("#saga_characters");
-const card = saga_characters.querySelector(".card");
+const ConjuntoPerfiles = document.querySelector("#ConjuntoPerfiles");
+const plantilla_perfil = document.querySelector(".Perfil");
 
-function CargarTodasLasSagas() {
-    fetch("http://localhost:3007/obtener_todas_las_sagas").then(recurso => recurso.json()).then(data => {
-        if (!data.sagas) {
-            return 0;
-        }
-        else{
-            for (let i = 0; i < data.sagas.length; i++) {
-                const saga = data.sagas[i];
-                const clon = container.cloneNode(true);
-                const iconImg = clon.querySelector("#icon");
-    
-                iconImg.onclick = function () {
-                    AbrirContainer(saga.saga_id);
-                };
-                const mimeType = 'image/jpeg';
-                const iconUrl = "data:" + mimeType + ";base64," + saga.link_icono;
-    
-                if (iconImg && saga.link_icono) {
-                    iconImg.src = iconUrl;
-                }
-    
-                card_place.appendChild(clon);
-            }
-        }
-    })
-}
+fetch("http://localhost:3000/obtener_id_perfiles").then(recurso => recurso.json()).then(respuesta => {
+    console.log(respuesta);
+    //CLON DE PLANTILLA DE PERFILES
+    for(i = 0; i < respuesta.ids.length; i++){
+        const clon = plantilla_perfil.cloneNode(true);
+        ConjuntoPerfiles.appendChild(clon);
 
-function CargarPersonajesPorSaga(sagaId) {
-    var closeButtonHtml = '';
+        fetch("http://localhost:3000/obtener_perfil_" + respuesta.ids[i].id).then(recurso => recurso.json()).then(respuesta => {
+            console.log(respuesta);
 
-    const closeButton = saga_characters.querySelector('button');
-    if (closeButton) {
-        closeButtonHtml = closeButton.outerHTML;
-    }
+            const Nombre = clon.querySelector(".Nombre");
+            Nombre.innerHTML = respuesta.nombre;
 
-    saga_characters.innerHTML = closeButtonHtml;
-
-    fetch("http://localhost:3007/obtener_info_saga_" + sagaId).then(recurso => recurso.json()).then(infoSaga => {
-        const saga_icon = icon_saga.querySelector("#saga_icon");
-        const img = 'image/jpeg';
-        const iconUrl = "data:" + img + ";base64," + infoSaga.link_icono;
-
-        if (saga_icon && infoSaga.link_icono) {
-            saga_icon.src = iconUrl;
-        } else {
-            console.log("Advertencia: Icono de saga no encontrado.");
-        }
-    });
-    fetch("http://localhost:3007/obtener_personajes_por_saga_" + sagaId).then(recurso => recurso.json()).then(data => {
-        if (!data.personajes) {
-            return 0;
-        }
-
-        for (let i = 0; i < data.personajes.length; i++) {
-            const personaje = data.personajes[i];
-            const clon = card.cloneNode(true);
-
-            clon.querySelector(".name_character").textContent = personaje.nombre_personaje;
-
-            const number_character = clon.querySelector(".number_character");
-            if (number_character) {
-                if (personaje.numero_smash) {
-                    number_character.textContent = "#" + personaje.numero_smash;
-                } else {
-                    number_character.textContent = "#" + '?';
-                }
-            }
-
-            const smash_character = clon.querySelector(".smash_character");
-            const img = 'image/jpeg';
-            const imageUrl = "data:" + img + ";base64," + personaje.link_personaje;
-
-            if (smash_character && personaje.link_personaje) {
-                smash_character.src = imageUrl;
-            }
-
-            clon.onclick = function () {
-                ElPersonaje(personaje.personaje_id);
+            if(respuesta.imagen != null){
+                const imagen = clon.querySelector("img");
+                const arregloBytes = new Uint8Array(respuesta.imagen.data);
+                const blob = new Blob([arregloBytes]);
+                var imagen_base64 = URL.createObjectURL(blob);
+                imagen.src = imagen_base64;
             };
-            saga_characters.appendChild(clon);
-        }
-    })
-}
+            clon.id = respuesta.id; 
 
-function CerrarContainer(){
-    if (container_saga) {
-        container_saga.style.display = "none";
+            clon.addEventListener("click", function(event){
+                window.location.href = "muro.html";
+                //console.log(event.currentTarget.id);
+                sessionStorage.setItem("muro_objetivo", respuesta.id);
+                //console.log(sessionStorage.getItem("muro_objetivo"));
+            });
+        });
     }
-}
-
-function AbrirContainer(sagaId){
-    if (container_saga) {
-        container_saga.style.display = "block";
-    }
-
-    if (sagaId) {
-        CargarPersonajesPorSaga(sagaId); 
-    } else {
-        console.warn("Advertencia: AbrirContainer fue llamado sin un 'sagaId'. No se cargarán personajes.");
-    }
-
-}
+    plantilla_perfil.remove();
+});
